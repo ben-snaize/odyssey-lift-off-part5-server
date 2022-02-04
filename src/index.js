343b30901809f9f60e2c54b5f3efb7229a7607e5
Ben Snaize <ben.snaize@frasers.group>
2022-02-04 10:42:59 +0000

    Slash

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const TrackAPI = require("./datasources/track-api");
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     cors: {
-      origin: "https://studio.apollographql.com/",
+      origin: "https://studio.apollographql.com",
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true,
     },
